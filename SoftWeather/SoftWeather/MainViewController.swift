fc91a6ca8fe0556012e55b3a250fd9ee45c49b59
AndruhaPower <pazit@yandex.ru>
2019-12-18 13:02:46 +0300

    initial commit (no storyboard)

---
+++ SoftWeather/SoftWeather/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SoftWeather
+//
+//  Created by Andrew on 18/12/2019.
+//  Copyright © 2019 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
